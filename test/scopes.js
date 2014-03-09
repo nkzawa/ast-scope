c54d1ac2403690b52ef4a74a4e9a2ab087b15499
Naoyuki Kanezawa <naoyuki.kanezawa@gmail.com>
2014-03-10 00:02:51 +0900

    distinguish scopes and variable scopes

--- test/scopes.js
+++ test/scopes.js
@@ -1,34 +1,33 @@
 var fs = require('fs');
 var expect = require('chai').expect;
 var esprima = require('esprima');
-var estraverse = require('estraverse');
-var Scope = require('../').Scope;
+var scopes = require('../').scopes;
 var findOne = require('./support').findOne;
 
 
-describe('Scope', function() {
+describe('scopes', function() {
   describe('isRequired', function() {
     it('should be true for Program', function() {
       var ast = esprima.parse('');
-      expect(Scope.isRequired(ast)).to.be.true;
+      expect(scopes.isRequired(ast)).to.be.true;
     });
 
     it('should be true for FunctionExpression', function() {
       var ast = esprima.parse('var foo = function() {};');
       var node = findOne(ast, 'FunctionExpression');
-      expect(Scope.isRequired(node)).to.be.true;
+      expect(scopes.isRequired(node)).to.be.true;
     });
 
     it('should be true for FunctionDeclaration', function() {
       var ast = esprima.parse('function foo() {};');
       var node = findOne(ast, 'FunctionDeclaration');
-      expect(Scope.isRequired(node)).to.be.true;
+      expect(scopes.isRequired(node)).to.be.true;
     });
 
     it('should be true for CatchClause', function() {
       var ast = esprima.parse('try {} catch (e) {}');
       var node = findOne(ast, 'CatchClause');
-      expect(Scope.isRequired(ast)).to.be.true;
+      expect(scopes.isRequired(ast)).to.be.true;
     });
   });
 });
